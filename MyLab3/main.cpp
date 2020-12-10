ba7dece5494fda270a91688e2a66793dcdde02b3
alexeysp11 <alexeyspiridon1111@gmail.com>
2020-12-10 23:59:12 +0500

    Edited Lab4 code

--- MyLab3/main.cpp
+++ MyLab3/main.cpp
@@ -17,34 +17,18 @@ int main()
 {  
   RCC::CR::HSEON::On::Set();
   while(!RCC::CR::HSERDY::Ready::IsSet());
- 
+
   RCC::CFGR::SW::Hse::Set();
   while(!RCC::CFGR::SWS::Hse::IsSet());
-  
+
   RCC::CR::HSION::Off::Set();
-  RCC::PLLCFGR::PLLSRC::HseSource::Set();
-  
-  // Set VCO equal to 2 MHz => HSE / 4 = 8'000'000 / 4 
-  RCC::PLLCFGR::PLLM0::Set(4U);
-  
-  // Set VCC Output equal to 128 MHz => VCO * 64 = 2'000'000 * 64
-  RCC::PLLCFGR::PLLN0::Set(64U);
-  
-  // Set PLL Output equal to 32 Mhz => VCC / 4 = 128'000'000 / 4
-  RCC::PLLCFGR::PLLP0::Pllp4::Set();
-  
-  RCC::CR::PLLON::On::Set();
-  while(RCC::CR::PLLRDY::Unclocked::IsSet());
-  
-  RCC::CFGR::SW::Pll::Set();
-  while(!RCC::CFGR::SWS::Pll::IsSet());
-  
+
   RCC::AHB1ENR::GPIOCEN::Enable::Set();
   GPIOC::MODER::MODER5::Output::Set();
   
   while(1)
   {
-    for(int i = 0; i < 100'000'000; i++);
+    for(int i = 0; i < 1'000'000; i++);
     GPIOC::ODR::ODR5::High::Set();
     
     for(int i = 0; i < 1'000'000; i++);
--- MyLab4/main.cpp
+++ MyLab4/main.cpp
@@ -1,98 +1,74 @@
 #include "rccregisters.hpp"
 #include "gpiocregisters.hpp"
-#include "gpioaregisters.hpp"
 #include <iostream>
-#include "tim2registers.hpp"
+#include "gpioaregisters.hpp"
 
-constexpr std::uint32_t CpuClock = 8'000'000U;
-
-auto delay_ms(std::uint32_t period)
+bool LEDsToggle(bool currentlyOn)
 {
-  // Update request source: 
-  // counter owerflow can generate an update interrupt
-  TIM2::CR1::URS::OverflowEvent::Set();
+  if(!currentlyOn)
+  {
+    GPIOC::BSRR::BS5::High::Write();
+    GPIOC::BSRR::BS9::High::Write();
+    GPIOC::BSRR::BS8::High::Write();
+    GPIOA::BSRR::BS5::High::Write();
+    currentlyOn = 1; 
+  }
+  else
+  {
+    GPIOC::BSRR::BR5::Low::Write();
+    GPIOC::BSRR::BR9::Low::Write();
+    GPIOC::BSRR::BR8::Low::Write();
+    GPIOA::BSRR::BR5::Low::Write();
+    currentlyOn = 0; 
+  }
   
-  const std::uint32_t timerValue = (CpuClock * 1000U) * period - 1U;
-  
-  // Wirte desired value to TIM2_ARR
-  TIM2::ARR::Write(timerValue);
-  
-  TIM2::SR::UIF::NoInterruptPending::Set();
-  
-  // Counter register
-  TIM2::CNT::Write(0U);  
-  
-  // Enable counter
-  TIM2::CR1::CEN::Enable::Set();
-  while(TIM2::SR::UIF::InterruptPending::IsSet());
-  
-  TIM2::CR1::CEN::Disable::Set();
-  
-  // Update interrupt flag
-  TIM2::SR::UIF::NoInterruptPending::Set();
+  return currentlyOn; 
 }
 
 int main()
 {
-  unsigned int ButPeriod = 100U;
-  unsigned int LedsPeriod = 500U;
-  unsigned int counter = 0;
-
-  bool flag = false;
-  
-  // Enable external HSE clock and wait until it's stable
   RCC::CR::HSEON::On::Set();
   while(!RCC::CR::HSERDY::Ready::IsSet());
-  
-  // Select HSE oscillator as system clock source
+
   RCC::CFGR::SW::Hse::Set();
   while(!RCC::CFGR::SWS::Hse::IsSet());
+  
+  RCC::CR::HSION::Off::Set();
 
-  // Set required pins and registers
-  RCC::APB1ENR::TIM2EN::Enable::Set();
+  RCC::PLLCFGR::PLLSRC::HseSource::Set();
+
+  RCC::PLLCFGR::PLLM0::Set(4U);
+  RCC::PLLCFGR::PLLN0::Set(50U);
+  RCC::PLLCFGR::PLLP0::Pllp6::Set();
+
+  RCC::CR::PLLON::On::Set();
+
+  while(RCC::CR::PLLRDY::Unclocked::IsSet());
+
+  RCC::CFGR::SW::Pll::Set();
+  while(!RCC::CFGR::SWS::Pll::IsSet());
+
   RCC::AHB1ENR::GPIOCEN::Enable::Set();
   RCC::AHB1ENR::GPIOAEN::Enable::Set();
   GPIOC::MODER::MODER13::Input::Set();
   GPIOC::MODER::MODER5::Output::Set();
-  GPIOA::MODER::MODER5::Output::Set();
-  GPIOC::MODER::MODER9::Output::Set();
   GPIOC::MODER::MODER8::Output::Set();
+  GPIOC::MODER::MODER9::Output::Set();
+  GPIOA::MODER::MODER5::Output::Set();
   
-  // Use block diagrams to implement main logic of the program!
+  bool isPressed = 0;
+  static bool currentlyOn = 0;
   while(1)
   {
-    // Implement a delay using timer TIM2
-    delay_ms(ButPeriod);
-    
-    // If user pushed a button 
-    if(GPIOC::IDR::IDR13::Low::IsSet())
+    if(GPIOC::IDR::IDR13::Low::IsSet() && !isPressed)
     {
-      LedsPeriod += 100U;
-      counter = 0;
+      isPressed = 1;
+      currentlyOn = LEDsToggle(currentlyOn);
     }
-    counter = counter + ButPeriod;
-    
-    if(counter >= LedsPeriod)
-    {
-      if(!flag)
-      {
-        GPIOC::BSRR::BS8::High::Write();
-        GPIOC::BSRR::BS9::High::Write();
-        GPIOC::BSRR::BS5::High::Write();
-        GPIOA::BSRR::BS5::High::Write();
-        flag = true;
-      }
-      else
-      {
-        GPIOC::BSRR::BR9::Low::Write();
-        GPIOC::BSRR::BR8::Low::Write();
-        GPIOC::BSRR::BR5::Low::Write();
-        GPIOA::BSRR::BR5::Low::Write();
-        flag = false;
-      }
-      
-      counter = 0;
-    }
+    if(GPIOC::IDR::IDR13::High::IsSet())
+      isPressed = 0;
+	
   }
-  return 1;
+
+  return 1 ;
 }
