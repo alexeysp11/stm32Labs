d84a0954357367961a07aa714b5b939d39acc71c
alexeysp11 <alexeyspiridon1111@gmail.com>
2020-12-16 09:27:28 +0500

    Edited Lab3

--- MyLab3/main.cpp
+++ MyLab3/main.cpp
@@ -1,37 +1,48 @@
 #include "rccregisters.hpp"
 #include "gpiocregisters.hpp"
 #include <iostream>
-#include <ctime>
-
-void sleep(float seconds)
-{
-    clock_t startClock = clock();
-    float secondsAhead = seconds * CLOCKS_PER_SEC;
-    
-    // do nothing until the elapsed time has passed.
-    while(clock() < startClock+secondsAhead);
-    return;
-}
 
 int main()
 {  
   RCC::CR::HSEON::On::Set();
   while(!RCC::CR::HSERDY::Ready::IsSet());
-
+ 
   RCC::CFGR::SW::Hse::Set();
   while(!RCC::CFGR::SWS::Hse::IsSet());
-
+  
   RCC::CR::HSION::Off::Set();
-
+  RCC::PLLCFGR::PLLSRC::HseSource::Set();
+  
+  // Set VCO equal to 2 MHz => HSE / 4 = 8'000'000 / 4 
+  RCC::PLLCFGR::PLLM0::Set(4U);
+  
+  // Set VCC Output equal to 128 MHz => VCO * 64 = 2'000'000 * 64
+  RCC::PLLCFGR::PLLN0::Set(64U);
+  
+  // Set PLL Output equal to 32 Mhz => VCC / 4 = 128'000'000 / 4
+  RCC::PLLCFGR::PLLP0::Pllp4::Set();
+  
+  RCC::CR::PLLON::On::Set();
+  while(RCC::CR::PLLRDY::Unclocked::IsSet());
+  
+  RCC::CFGR::SW::Pll::Set();
+  while(!RCC::CFGR::SWS::Pll::IsSet());
+  
   RCC::AHB1ENR::GPIOCEN::Enable::Set();
   GPIOC::MODER::MODER5::Output::Set();
   
   while(1)
   {
-    for(int i = 0; i < 1'000'000; i++);
+    for(int i = 0; i < 1'250'000; i++)
+    {
+      asm volatile("");
+    }
     GPIOC::ODR::ODR5::High::Set();
     
-    for(int i = 0; i < 1'000'000; i++);
+    for(int i = 0; i < 1'250'000; i++)
+    {
+      asm volatile("");
+    }
     GPIOC::ODR::ODR5::Low::Set();
   }
   return 1 ;
--- MyLab4/main.cpp
+++ MyLab4/main.cpp
@@ -1,44 +1,69 @@
-// #include <iostream>
 #include "rccregisters.hpp"
-#include "button.hpp"
+#include "gpiocregisters.hpp"
+#include <iostream>
+#include "gpioaregisters.hpp"
 
-extern "C"
+void LEDsToggle()
 {
-  int __low_level_init(void)
-  {
-    RCC::CR::HSEON::On::Set();
-    while(!RCC::CR::HSERDY::Ready::IsSet());
-
-    RCC::CFGR::SW::Hse::Set();
-    while(!RCC::CFGR::SWS::Hse::IsSet());
-    
-    RCC::CR::HSION::Off::Set();
-
-    RCC::PLLCFGR::PLLSRC::HseSource::Set();
-
-    RCC::PLLCFGR::PLLM0::Set(4U);
-    RCC::PLLCFGR::PLLN0::Set(50U);
-    RCC::PLLCFGR::PLLP0::Pllp6::Set();
-
-    RCC::CR::PLLON::On::Set();
-
-    while(RCC::CR::PLLRDY::Unclocked::IsSet());
-
-    RCC::CFGR::SW::Pll::Set();
-    while(!RCC::CFGR::SWS::Pll::IsSet());
-    
-    return 1; 
-  }
+  
 }
 
 int main()
 {
-  Button<GPIOC> button; 
-  
+  RCC::CR::HSEON::On::Set();
+  while(!RCC::CR::HSERDY::Ready::IsSet());
+
+  RCC::CFGR::SW::Hse::Set();
+  while(!RCC::CFGR::SWS::Hse::IsSet());
+
+  RCC::CR::HSION::Off::Set();
+
+  RCC::PLLCFGR::PLLSRC::HseSource::Set();
+
+  RCC::PLLCFGR::PLLM0::Set(4U);
+  RCC::PLLCFGR::PLLN0::Set(50U);
+  RCC::PLLCFGR::PLLP0::Pllp6::Set();
+
+  RCC::CR::PLLON::On::Set();
+
+  while(RCC::CR::PLLRDY::Unclocked::IsSet());
+
+  RCC::CFGR::SW::Pll::Set();
+  while(!RCC::CFGR::SWS::Pll::IsSet());
+
+  RCC::AHB1ENR::GPIOCEN::Enable::Set();
+  RCC::AHB1ENR::GPIOAEN::Enable::Set();
+  GPIOC::MODER::MODER13::Input::Set();
+  GPIOC::MODER::MODER5::Output::Set();
+  GPIOC::MODER::MODER8::Output::Set();
+  GPIOC::MODER::MODER9::Output::Set();
+  GPIOA::MODER::MODER5::Output::Set();
+
+  bool isPressed = false;
   while(1)
   {
-    button.ReactOnPushing();
+    if(GPIOC::IDR::IDR13::Low::IsSet() && isPressed)
+    {
+      isPressed = false;
+      if(GPIOC::ODR::ODR5::Low::IsSet())
+      {
+        GPIOC::BSRR::BS5::High::Write();
+        GPIOC::BSRR::BS9::High::Write();
+        GPIOC::BSRR::BS8::High::Write();
+        GPIOA::BSRR::BS5::High::Write();
+      }
+      else
+      {
+        GPIOC::BSRR::BR5::Low::Write();
+        GPIOC::BSRR::BR9::Low::Write();
+        GPIOC::BSRR::BR8::Low::Write();
+        GPIOA::BSRR::BR5::Low::Write();
+      }
+    }
+
+    if(GPIOC::IDR::IDR13::High::IsSet())
+      isPressed = true;
   }
 
-  return 0 ;
+  return 1 ;
 }
